e896216bcc522995ef023b7800c14b02416eb401
milano153 <punksonsomnia@gmail.com>
2025-10-18 12:50:04 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -650,7 +650,7 @@ if (isMobile) {
       <StyledLogo
               src={"/config/images/logo.png"}
                 />
-      <h1 style={{ fontSize: "28px", marginBottom: "10px" }}>Our mint website is currently designed for desktop</h1>
+      <h1 style={{ fontSize: "28px", marginBottom: "10px", marginTop: "20px" }}>Our mint website is currently designed for desktop</h1>
       <p style={{ fontSize: "18px" }}>A mobile version is coming soon!</p>
     </div>
   );
